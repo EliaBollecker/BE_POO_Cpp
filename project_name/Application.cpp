3e9fc69749f32385b0efbb1ef6326891177dc0bd
EliaBollecker <elia.bollecker@gmail.com>
2024-11-14 14:10:48 +0100

    retour du fichier

---
+++ project_name/Application.cpp
@@ -0,0 +1,32 @@
+/*********************************************************************
+ * @file  Apllication.cpp
+ * @author <Elia Bollecker-Fevre et Elise Weltzer>
+ * @brief Fichier source de l'application
+ *********************************************************************/
+#include "Application.h"
+
+
+Application::Application()
+{
+  // Code
+  ; 
+}
+  
+Application::~Application()
+{
+  // Code
+  ;
+}  
+
+void Application::init(void)
+{
+  // Code
+    ;
+}
+
+
+void Application::run(void)
+{
+  // Code
+    ;
+}
---
+++ project_name/Application.h
@@ -0,0 +1,37 @@
+/*********************************************************************
+ * @file  Apllication.h
+ * @author <Elia Bollecker-Fevre et Elise Weltzer>
+ * @brief Fichier header de l'application
+ *********************************************************************/
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+/**
+  * @class Application
+  * @brief Classe Application 
+*/    
+class Application
+{
+  public :
+    /**
+     * @fn Application();
+     * @brief Constructeur par defaut
+    */    
+    Application();
+    /**
+     * @fn Application();
+     * @brief Destructeur
+    */    
+    ~Application();    
+    /**
+     * @fn void init(void)
+     * @brief Fonction d'initialisation de l'application
+    */
+    void init(void);
+    /**
+     * @fn void run(void)
+     * @brief Fonction de lancement de l'application
+    */
+    void run(void);
+};
+#endif
