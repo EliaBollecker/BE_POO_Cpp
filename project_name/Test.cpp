6a1c9cc007314d84c0a00ea3cfd48951b0187fc9
EliaBollecker <elia.bollecker@gmail.com>
2024-11-14 14:23:59 +0100

    ajout classe test

---
+++ project_name/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.h"
+
+Test::Test(){
+    ;
+}
+
+
+void Test::ma_fonction(void){
+    ;
+}
---
+++ project_name/Test.h
@@ -0,0 +1,22 @@
+/*********************************************************************
+ * @file  Test.h
+ * @author <Elia Bollecker-Fevre et Elise Weltzer>
+ * @brief Fichier header de la classe de test
+ *********************************************************************/
+#ifndef TEST_H_
+#define TEST_H_
+
+class Test {
+    private :
+    int p ;
+    public :
+    Test();
+    void ma_fonction(void);
+
+};
+
+
+
+
+
+#endif
