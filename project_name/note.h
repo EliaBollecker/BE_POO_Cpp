8995b444bb87c0e6999c98c6c4b0bc6f5e851b2c
Weltzer Elise <weltzer@insa-11277>
2024-11-19 09:22:40 +0100

    note

--- project_name/note.h
+++ project_name/note.h
@@ -1,11 +1,16 @@
+/*********************************************************************
+ * @file  note.h
+ * @author <Elia Bollecker-Fevre et Elise Weltzer>
+ * @brief Fichier header de Note permet de stocker toutes les notes possibles 
+ *********************************************************************/
 #ifndef NOTE_H
 #define NOTE_H
-//autrices Elia Bollecker-Fevre et Elise Weltzer 
 #include <iostream>
 
 //la class note permet de définir toutes les constantes des notes jouables en fréquences (Hz)
-class note {
+class Note {
     protected : 
+    //notes 
         float const do1 = 261.63; 
         float const re = 293.66; 
         float const mi = 329.63 ; 
@@ -14,9 +19,11 @@ class note {
         float const la = 440 ; 
         float const si = 493.88; 
         float const do2 = 523.25; 
+    //mesure 
+        float mesure ; 
     public : 
 
-        virtual float getNote(float mesure) = 0 ; //methode abstraite permettant d'avoir la note en fonction de la mesure 
+        virtual float getNote() = 0 ; //methode abstraite permettant d'avoir la note en fonction de la mesure 
 
 };
 
